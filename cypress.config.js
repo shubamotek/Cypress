1f35b646a860a0da2ed19b558ea304deec828f5b
shubamotek <shub.prasain@amotek.be>
2023-02-21 09:56:17 +0100

    aangepast

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  videoCompression : 15,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
