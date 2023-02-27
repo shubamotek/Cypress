7621bac6f44579ec1100c1cc407bb7e28694a012
shubamotek <shub.prasain@amotek.be>
2023-02-27 10:36:34 +0100

    push

--- cypress/e2e/Supplier.cy.js
+++ cypress/e2e/Supplier.cy.js
@@ -92,25 +92,7 @@ describe("test", () => {
 //  cy.contains('Cancel').click(  
   
 
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  
-
-  
-afterEach(() => {
-  if (this.currentTest.state === 'failed') {
-    // If the test failed, send an email with SendGrid
-    const msg = {
-      to: 'shubprasain@hotmail.com', // Replace with recipient's email address
-      from: 'shubprasain@amotek.be', // Replace with your email address
-      subject: `Test Failed: ${this.currentTest.fullTitle()}`,
-      text: 'One or more tests failed. Please check the test results for more details.',
-      html: '<p>One or more tests failed. Please check the test results for more details.</p>',
-    };
-    sgMail.send(msg);
-  }
-});
   //Upload images
   
   
