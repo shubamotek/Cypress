25423c534b12b8cd896f68dd00fc8f72f160368b
shubamotek <shub.prasain@amotek.be>
2023-02-27 11:14:41 +0100

    nieuwe push

--- cypress/e2e/Help.cy.js
+++ cypress/e2e/Help.cy.js
@@ -12,15 +12,16 @@ describe("test", () => {
     cy.contains('Help').click();
     cy.contains('Report bug').click();
     cy.xpath('//*[@id="Widget884038691"]').type('team@amotek.be')
-  //  cy.xpath('//*[@id="Widget1730235547"]').click()
-  //  cy.get('#Widget1730235547').select('Bug report');
-
-   cy.get('table').contains('td', '8');
+    cy.get('#Widget1730235547').select('Bug report' ,{force:true})
+  //  cy.xpath('//*[@id="entry.1747016377.6"]').select('6',{force:true})
   // Priority
   cy.xpath('//*[@id="Widget2064489387"]').type('Test')
   cy.xpath('//*[@id="Widget77187006"]').type('Dit is een test *IGNORE*')
   cy.xpath('//*[@id="Display1067499710"]/a').click()
   cy.contains('Add file').click()
+  cy.get('')
+
+
 
   
   
