bc5762866b02e73491e6dc5ec4b5feb286daaea0
shubamotek <shub.prasain@amotek.be>
2023-02-28 14:57:59 +0100

    pushhh

--- cypress/e2e/NewEvent.cy.js
+++ cypress/e2e/NewEvent.cy.js
@@ -172,9 +172,9 @@ describe("test", () => {
     cy.contains('Events').click()
     cy.get('.ant-dropdown-trigger.ant-table-filter-trigger').eq(0).click();
     cy.get('input.ant-input').eq(1)
-    .type('Testingggg');
+    .type('Meeting');
     cy.contains('Search').click({force:true})
-    cy.xpath('//*[@id="root"]/section/section/main/div/div[2]/div/div/div/div/div/div/table/tbody/tr[1]/td[6]/div/div[2]').click()
+   cy.xpath('//*[@id="root"]/section/section/main/div/div[2]/div/div/div/div/div/div/table/tbody/tr[1]/td[6]/div/div[2]').click()
 
      cy.xpath('/html/body/div[6]/div/div/div/div[2]/div/div[2]/button[2]').click()
      cy.contains('Delete').click()
