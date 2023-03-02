f0f3ec2ef9024c804d28db3c02b111dadb706af1
shubamotek <shub.prasain@amotek.be>
2023-03-02 10:15:00 +0100

    push

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   videoCompression : 15,
-  projectId : "suqo8p",
+  projectId : "7efwk2",
   e2e: {
     baseUrl : "https://heroku-cocomo-frontend-dev.herokuapp.com/",
     setupNodeEvents(on, config) {
